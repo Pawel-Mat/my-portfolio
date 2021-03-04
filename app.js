2bc6e9af314dcb949b59f2d4b9306ae48e295e36
Paweł Matysiak <mat.pawel@gmail.com>
2021-03-04 22:40:16 +0100

    Update

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ gsap.registerPlugin(ScrollTrigger, TextPlugin)
 
 const tl = gsap.timeline({defaults: {ease: "linear"}});
 
-// tl.to('.text', {y:"0%", duration: 0.1, stagger: 0.25});
-// tl.to('.text1', {duration: 1, text: "// it's up to you..."});
-// tl.to('.text2', {duration: 1, text: "if(youWant() === true){"});
-// tl.to('.text3', {duration: 0.7, text: "&nbsp&nbsp youCan();"});
-// tl.to('.text4', {duration: 0.5, delay: 0.5, text: "}else{"});
-// tl.to('.text5', {duration: 1, delay: 0.5, text: "youCant();}"});
-// tl.to('.slider', {y:"-100%", duration: 1, delay: 0.5});
-// tl.to('.intro', {y:"-100%", duration: 1},"-=1");
+tl.to('.text', {y:"0%", duration: 0.1, stagger: 0.25});
+tl.to('.text1', {duration: 1, text: "// it's up to you..."});
+tl.to('.text2', {duration: 1, text: "if(youWant() === true){"});
+tl.to('.text3', {duration: 0.7, text: "&nbsp&nbsp youCan();"});
+tl.to('.text4', {duration: 0.5, delay: 0.5, text: "}else{"});
+tl.to('.text5', {duration: 1, delay: 0.5, text: "youCant();}"});
+tl.to('.slider', {y:"-100%", duration: 1, delay: 0.5});
+tl.to('.intro', {y:"-100%", duration: 1},"-=1");
 tl.fromTo('.page-title, .page-subtitle, nav', {y:"30%", opacity: 0}, {y:"0%", opacity:1, duration: 2 });
 
 gsap.from('.my-pic', {scrollTrigger: {
@@ -36,7 +36,7 @@ const tlProjects = gsap.timeline({scrollTrigger: {
 
 tlProjects.from('.project-left', {x:"-50", duration: 1, opacity: 0})
 tlProjects.from('.project-right', {x:"50", duration: 1, opacity: 0},"-=1")
-tlProjects.from('.project-description', {y:"70", duration: 0.5, opacity: 0})
+tlProjects.from('.project-description', {y:"70", duration: 0.5, opacity: 0}, "-=0.3")
 
 const tlSkills = gsap.timeline({scrollTrigger: {
   trigger: ".skills-subtitle", 
@@ -60,9 +60,11 @@ gsap.from('.page-footer', {scrollTrigger: {
   toggleActions:"play reverse play reverse"},
   y:"50px", duration: 1})
 
+  
+
 window.addEventListener("scroll",  () => {
-  const navSticky = document.querySelector("nav");
-  navSticky.classList.toggle("nav-sticky", window.scrollY > 0)
+  const navFixed = document.querySelector("nav");
+  navFixed.classList.toggle("nav-fixed", window.scrollY > 0)
 })
 
 const hamburger = document.querySelector('.hamburger');
